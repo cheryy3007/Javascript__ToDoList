07034a4a8ccc7eb394923c5b4f4edea373c483b2
cheryy3007 <zulfiyagazna@gmail.com>
2025-01-08 22:25:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById("send");
+const btn = document.getElementById("daynight");
+const input = document.getElementById("input");
+const taskList = document.getElementById("taskList");
+const body = document.querySelector("body")
+
+
+btn.addEventListener("click", () => {
+    if(document.body.classList.toggle("dark-mode")){
+        btn.textContent = '☀️';
+    }else{
+        btn.textContent = '🌙';
+    }
+});
+
+
